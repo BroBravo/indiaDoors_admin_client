2750f1e631df46a22f987b9622b0e23ab1c9abf8
Uzair <qureshiuzair06@gmail.com>
2025-07-10 01:26:06 +0530

    Initial commit

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+// postcss.config.cjs (CommonJS syntax)
+const tailwindcss = require('@tailwindcss/postcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [tailwindcss(), autoprefixer],
+};
+
