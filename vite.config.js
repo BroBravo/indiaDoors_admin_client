a622f4f866f13d066b6572c05cadf0e4cb52895c
Uzair <qureshiuzair06@gmail.com>
2025-09-21 18:31:03 +0530

    env file added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,   // 👈 always use 3000
+    strictPort: true // 👈 fail if 3000 is busy (instead of picking another)
+  }
 })
