1ab1add665d9f8ac104415487ad42782b46d26bb
Uzair <qureshiuzair06@gmail.com>
2025-10-05 20:12:44 +0530

    user context path correction

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useUser } from "../context/userContext";
+import { useUser } from "../../context/userContext";
 import "./index.css";
 
 function LoginPage() {
